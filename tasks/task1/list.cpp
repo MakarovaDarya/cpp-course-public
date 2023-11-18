354ffcde93be8f9c8d2493110cd94f3553fc2155
Makarova Darya <makarovadarya2704@gmail.com>
2023-11-19 00:50:47 +0300

    task1

--- tasks/task1/list.cpp
+++ tasks/task1/list.cpp
@@ -3,21 +3,32 @@
 
 List::List()
 {
-
+	first = nullptr;
+	last = nullptr;
 }
 
 
 bool
 List::is_empty()
 {
-
+	if (first == nullptr) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 
 void
 List::clear()
 {
-
+	Node* current = first;
+	while (current != nullptr) {
+		Node* temp = current;
+		current = current->next;
+		delete temp;
+	}
+	first = nullptr;
 }
 
 
@@ -25,13 +36,26 @@ List::clear()
 void
 List::push_front(int _val)
 {
-
+	Node* new_node = new Node(_val);
+	new_node->next = first;
+	first = new_node;
 }
 
 
 void
 List::push_back(int _val)
 {
+	Node* new_node = new Node(_val);
+	if (is_empty()) {
+		first = new_node;
+
+	}
+	else {
+		Node* current = first;
+		while (current->next != nullptr) {
+			current->next = new_node;
+		}
+	}
 
 }
 
@@ -39,6 +63,15 @@ List::push_back(int _val)
 Node*
 List::find(int _val)
 {
+	Node* current = first;
+	while (current != nullptr) {
+		if (current->val == _val) {
+			return current;
+		}
+		current = current->next;
+	}
+	return nullptr;
+
 
 }
 
@@ -46,19 +79,54 @@ List::find(int _val)
 void
 List::remove_front()
 {
-
+	if (is_empty()) {
+		return;
+	}
+	Node* temp = first;
+	first = first->next;
+	delete temp;
 }
 
 
 void
 List::remove_back()
 {
-
+	if (!is_empty()) {
+		if (first->next == nullptr) {
+			delete first;
+			first = nullptr;
+		}
+		else {
+			Node* current = first;
+			while (current->next->next != nullptr) {
+				current = current->next;
+			}
+			delete current->next;
+			current->next = nullptr;
+		}
+	}
 }
 
 
 bool
 List::remove(const Node* _node)
 {
-
+	if (_node == nullptr) {
+		return false;
+	}
+	if (first == _node) {
+		remove_front();
+		return true;
+	}
+	Node* current = first;
+	while (current->next != nullptr) {
+		if (current->next == _node) {
+			Node* temp = current->next;
+			current->next = temp->next;
+			delete temp;
+			return true;
+		}
+		current = current->next;
+	}
+	return false;
 }
