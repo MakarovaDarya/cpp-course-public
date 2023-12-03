6a30fcd729be95b2bbdb91f4fe537c3c354abd85
Makarova Darya <makarovadarya2704@gmail.com>
2023-12-03 04:57:02 +0300

    задание3

--- tasks/task3/bin_search.hpp
+++ tasks/task3/bin_search.hpp
@@ -1,5 +1,22 @@
 #pragma once
 
 int* LowerBound(int* first, int* last, int value) {
-	throw std::runtime_error("not implemented error");
+	int* current;
+	int count = std::distance(first, last);
+	int step = count;
+
+	while (count > 0) {
+		current = first;
+		step = count / 2;
+		std::advance(current, step);
+
+		if (*current < value) {
+			first = ++current;
+			count -= step + 1;
+		}
+		else {
+			count = step;
+		}
+	}
+	return first;
 }
--- tasks/task3/main.cpp
+++ tasks/task3/main.cpp
@@ -1,9 +1,21 @@
-#include "bin_search.hpp"
-
 #include "iostream"
+#include "bin_search.hpp"
 
 int main()
 {
+    int arr[] = { 1, 2, 4 , 6, 8, 10 };
+    int digit;
+    std::cin >> digit;
+    int* result = LowerBound(std::begin(arr), std::end(arr), digit);
+
+    if (result != std::end(arr)) {
+        std::cout << "Первое большее или равное число: " << *result << std::endl;
+    }
+    else {
+        std::cout << "Большего числа нет" << std::endl;
+    }
+
+    return 0;
     // nullptr - не должны падать, вернуть nullptr
     // ТУТ ЗАГЛУШКА, ЧТОБЫ КОД КОМПИЛИРОВАЛСЯ
     LowerBound(nullptr, nullptr, 0);
